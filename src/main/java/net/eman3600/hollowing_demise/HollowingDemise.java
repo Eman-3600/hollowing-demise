4d06c8008dee0a8b21b39ced2d2db342c7edab56
Eman3600 <real.eman3600@gmail.com>
2025-12-11 23:42:31 -0500

    Imported Cardinal Components (Entity)

--- src/main/java/net/eman3600/hollowing_demise/HollowingDemise.java
+++ src/main/java/net/eman3600/hollowing_demise/HollowingDemise.java
@@ -6,12 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HollowingDemise implements ModInitializer {
-	public static final String MOD_ID = "hollowing_demise";
+	public static final String MODID = "hollowing_demise";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/net/eman3600/hollowing_demise/HollowingDemiseClient.java
@@ -0,0 +1,10 @@
+package net.eman3600.hollowing_demise;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HollowingDemiseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
