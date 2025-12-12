2dd23f1a8cf60b3f49ab0a7e6e8f08737f37c333
Eman3600 <real.eman3600@gmail.com>
2025-12-11 22:37:35 -0500

    Initial Commit

---
+++ src/main/java/net/eman3600/hollowing_demise/HollowingDemiseDataGenerator.java
@@ -0,0 +1,11 @@
+package net.eman3600.hollowing_demise;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HollowingDemiseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
