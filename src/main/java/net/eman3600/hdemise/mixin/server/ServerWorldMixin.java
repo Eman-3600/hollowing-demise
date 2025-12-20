885657f5767ef5460123c7e7f02777457f6ce65b
ThereIsProblemo <157442081+ThereIsProblemo@users.noreply.github.com>
2025-12-19 22:21:06 -0500

    Added "Sleeping through this day" text
    - Adds a mixin redirect to change the "Sleeping through this night" text to "Sleeping through this day" if it is daytime.

--- src/main/java/net/eman3600/hdemise/mixin/server/ServerWorldMixin.java
+++ src/main/java/net/eman3600/hdemise/mixin/server/ServerWorldMixin.java
@@ -4,17 +4,26 @@ import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import net.minecraft.world.EntityLookupView;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin extends World implements EntityLookupView, StructureWorldAccess {
+
+    @Unique
+    private boolean isDayTime() {
+        return isDay() & this.properties.getTimeOfDay() % 24000L < 12000L;
+    }
 
     protected ServerWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, boolean isClient, boolean debugWorld, long seed, int maxChainedNeighborUpdates) {
         super(properties, registryRef, registryManager, dimensionEntry, isClient, debugWorld, seed, maxChainedNeighborUpdates);
@@ -25,6 +34,15 @@ public abstract class ServerWorldMixin extends World implements EntityLookupView
         long t = isDay() ? 12000L : 24000L;
         long timeOfDay = this.properties.getTimeOfDay();
         long l = timeOfDay + t;
-        return (l - l % t) + (isDay() & timeOfDay % 24000L < 12000L ? 1000L : 0L);
+        return (l - l % t) + (isDayTime() ? 1000L : 0L);
+    }
+
+    @Redirect(method = "sendSleepingStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;translatable(Ljava/lang/String;)Lnet/minecraft/text/MutableText;"))
+    private MutableText hdemise$translatable(String key) {
+        MutableText text = Text.translatable(key);
+        if(isDayTime() && key.equals("sleep.skipping_night")) {
+            text = Text.literal("Sleeping through this day");
+        }
+        return text;
     }
 }
